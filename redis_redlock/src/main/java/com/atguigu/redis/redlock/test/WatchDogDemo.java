d0a30b0fc9fb84972d69358df9e90a37a67c82f7
ly10228 <601520157@qq.com>
2021-06-06 16:10:58 +0800

    canal保证数据最终一致性

--- redis_redlock/src/main/java/com/atguigu/redis/redlock/test/WatchDogDemo.java
+++ redis_redlock/src/main/java/com/atguigu/redis/redlock/test/WatchDogDemo.java
@@ -10,8 +10,7 @@ import java.util.concurrent.TimeUnit;
  * @auther zzyy
  * @create 2021-05-24 15:45
  */
-public class WatchDogDemo
-{
+public class WatchDogDemo {
     public static final String LOCKKEY = "AAA";
 
     private static Config config;
@@ -19,25 +18,26 @@ public class WatchDogDemo
 
     static {
         config = new Config();
-        config.useSingleServer().setAddress("redis://"+"192.168.111.147"+":6379").setDatabase(0);
-        redisson = (Redisson)Redisson.create(config);
+        config.useSingleServer().setAddress("redis://" + "192.168.0.122" + ":6379").setDatabase(0);
+        redisson = (Redisson) Redisson.create(config);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         RLock redissonLock = redisson.getLock(LOCKKEY);
 
         redissonLock.lock();
-        try
-        {
+        try {
             System.out.println("1111-------biz");
             //暂停几秒钟线程
-            try { TimeUnit.SECONDS.sleep(25); } catch (InterruptedException e) { e.printStackTrace(); }
-        }catch (Exception e){
+            try {
+                TimeUnit.SECONDS.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(redissonLock.isLocked() && redissonLock.isHeldByCurrentThread())
-            {
+        } finally {
+            if (redissonLock.isLocked() && redissonLock.isHeldByCurrentThread()) {
                 redissonLock.unlock();
             }
         }
@@ -45,7 +45,11 @@ public class WatchDogDemo
         System.out.println(Thread.currentThread().getName() + " main ------ ends.");
 
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         redisson.shutdown();
     }
 }
